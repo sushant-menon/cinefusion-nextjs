5727c0dcde172c176287cc6abc4d69ad49f69e34
Sushant <sushantnambiar@gmail.com>
2023-09-10 02:15:30 +0530

    made layout for sidebar and logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex">
+      <h1 className="font-bold text-3xl">CINEFUSION</h1>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <Image
+        src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
+        alt=""
+        width={40}
+        height={37}
+      />
+    </div>
+  );
+};
+
+export default Sidebar;
