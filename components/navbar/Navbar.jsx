5ba19d5a79a7b063fe08385fd761edb860623b7b
sushant-menon <sushantnambiar@gmail.com>
2023-12-27 01:59:42 +0530

    added search functionality

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,47 +1,55 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { toggleSidebar } from "@/redux/features/appslice";
 import { useDispatch } from "react-redux";
 import Link from "next/link";
 
 const Navbar = ({ toggle }) => {
+  const [searchValue, setSearchValue] = useState("");
+
   const dispatch = useDispatch();
   const toggleSidebarMenu = () => {
     dispatch(toggleSidebar());
   };
 
-  return (
-    <div className="flex bg-white/40 px-3 py-5 justify-between items-center">
-      <button onClick={() => toggleSidebarMenu()}>
-        <Image
-          src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
-          alt=""
-          width={40}
-          height={37}
-        />
-      </button>
-      <Link href="/">
-        <h1 className="font-bold text-3xl"> CINEFUSION </h1>
-      </Link>
+  const handleSubmit = e => {
+    e.preventDefault();
+    setSearchValue("");
+  };
 
-      <div className="flex items-center">
-        <input
-          className="border-4 border-r-0 border-black bg-gray-600 hover:bg-gray-700 text-gray-200 px-2 py-1  rounded-xl rounded-r-none outline-none"
-          type="search"
-          placeholder="Search"
-        />
-        <button
-          className="border-4 border-black hover:bg-blue-800 bg-gray-600 px-3 py-1 rounded-l-none rounded-xl font"
-          type="submit"
-        >
-          <img src="/search (2).svg" />
+  return (
+    <>
+      <div className="flex bg-white/40 px-3 py-5 justify-between items-center">
+        <button onClick={() => toggleSidebarMenu()}>
+          <Image
+            src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
+            alt=""
+            width={40}
+            height={37}
+          />
+        </button>
+        <Link href="/">
+          <h1 className="font-bold text-3xl"> CINEFUSION </h1>
+        </Link>
+
+        <form onSubmit={handleSubmit} className="flex items-center">
+          <input
+            value={searchValue}
+            onChange={e => setSearchValue(e.target.value)}
+            className="border-2 border-black border-r-0 px-3 py-2 rounded-xl rounded-r-none bg-gray-800 text-white/70 outline-none hover:bg-gray-700"
+            placeholder="Search"
+          />
+          <button className="rounded-xl rounded-l-none border-2 border-black px-3 py-2 hover:bg-blue-600">
+            <img src="/search (2).svg" alt="" />
+          </button>
+        </form>
+
+        <button className="border border-black bg-gray-800 text-white/60 px-2 py-2 font-bold hover:bg-red-600 hover:text-white rounded-lg">
+          Sign Up
         </button>
       </div>
-      <button className="border border-black bg-gray-800 text-white/60 px-2 py-2 font-bold hover:bg-red-600 hover:text-white rounded-lg">
-        Sign Up
-      </button>
-    </div>
+    </>
   );
 };
 
