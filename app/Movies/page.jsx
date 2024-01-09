1c819e38a1bda8452c49256185dd65a1f9e5638b
Sushant Menon <sushantnambiar@gmail.com>
2024-01-09 23:42:02 +0530

    updated

--- app/Movies/page.jsx
+++ app/Movies/page.jsx
@@ -4,9 +4,10 @@ import React from "react";
 
 const movies = () => {
   return (
-    <div className="h-[90vh] overflow-auto overflow-x-hidden mx-0 mt-8 flex flex-wrap justify-around pl-2">
-      <MovieList />
-    </div>
+    // <div className="h-[90vh] overflow-auto overflow-x-hidden mx-0 mt-8 flex flex-wrap justify-around pl-2">
+    //   <MovieList />
+    // </div>
+    <div>Hello World</div>
   );
 };
 
