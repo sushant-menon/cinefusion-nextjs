ab259251b087401999fdad1754007d0f44250faf
Sushant Menon <sushantnambiar@gmail.com>
2024-01-09 22:26:22 +0530

    updated

--- app/tvshows/page.jsx
+++ app/tvshows/page.jsx
@@ -3,9 +3,10 @@ import React from "react";
 
 const tvshow = () => {
   return (
-    <div className="h-[90vh] scrollbar-thin  scrollbar-track-gray-800 scrollbar-thumb-rounded-md overflow-auto overflow-x-hidden mx-1 mt-8 flex flex-wrap justify-around">
-      <TvShowList />
-    </div>
+    // <div className="h-[90vh] scrollbar-thin  scrollbar-track-gray-800 scrollbar-thumb-rounded-md overflow-auto overflow-x-hidden mx-1 mt-8 flex flex-wrap justify-around">
+    //   <TvShowList />
+    // </div>
+    <div>Hello WOrld</div>
   );
 };
 
