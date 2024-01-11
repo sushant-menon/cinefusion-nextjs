bef96e5c5fe2eede8617696f8d289b53f0135fb3
Sushant Menon <sushantnambiar@gmail.com>
2024-01-12 00:19:51 +0530

    updated

--- app/Movies/page.jsx
+++ app/Movies/page.jsx
@@ -14,8 +14,9 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div className="h-[150vh] overflow-auto overflow-x-hidden mx-0 pt-8 flex flex-wrap justify-around pl-2">
-      <MovieList />
-    </div>
+    // <div className="h-[150vh] overflow-auto overflow-x-hidden mx-0 pt-8 flex flex-wrap justify-around pl-2">
+    //   <MovieList />
+    // </div>
+    <div>Hello World</div>
   );
 }
