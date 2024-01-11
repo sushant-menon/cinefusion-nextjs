c52d1132314bf90bc4ddb796f37736064b7c17d3
Sushant Menon <sushantnambiar@gmail.com>
2024-01-12 00:22:42 +0530

    updated

--- app/tvshows/page.jsx
+++ app/tvshows/page.jsx
@@ -3,10 +3,10 @@
 
 const tvShow = () => {
   return (
-    // <div className="h-[90vh] scrollbar-thin  scrollbar-track-gray-800 scrollbar-thumb-rounded-md overflow-auto overflow-x-hidden mx-1 mt-8 flex flex-wrap justify-around">
-    //   <TvShowList />
-    // </div>
-    <div>Hello</div>
+    <div className="h-[90vh] scrollbar-thin  scrollbar-track-gray-800 scrollbar-thumb-rounded-md overflow-auto overflow-x-hidden mx-1 mt-8 flex flex-wrap justify-around">
+      <TvShowList />
+    </div>
+    // <div>Hello</div>
   );
 };
 
