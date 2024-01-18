48d8ffca6ae17a120c70574417d4aaf6f62b2ea5
Sushant Menon <sushantnambiar@gmail.com>
2024-01-19 00:17:46 +0530

    modified home page

--- app/page.js
+++ app/page.js
@@ -136,15 +136,17 @@ export default function Home() {
                     <p className="text-sm font-medium lg:text-lg lg:font-bold text-gray-900 dark:text-gray-300">
                       Day
                     </p>
-                    <label class="relative inline-flex items-center cursor-pointer">
+                    <label className="relative inline-flex items-center cursor-pointer">
                       <input
                         onClick={changeCategoryButton}
+                        onTouchStart={changeCategoryButton}
+                        onTouchEnd={changeCategoryButton}
                         type="checkbox"
                         value={currentCategory}
-                        class="sr-only peer"
+                        className="sr-only peer"
                       />
-                      <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                      <span class="ms-3 lg:text-lg lg:font-bold font-medium text-sm text-gray-900 dark:text-gray-300">
+                      <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                      <span className="ms-3 lg:text-lg lg:font-bold font-medium text-sm text-gray-900 dark:text-gray-300">
                         Week
                       </span>
                     </label>
@@ -153,15 +155,15 @@ export default function Home() {
                     <p className="text-sm font-medium lg:text-lg lg:font-bold text-gray-900 dark:text-gray-300">
                       Tv
                     </p>
-                    <label class="relative inline-flex items-center cursor-pointer">
+                    <label className="relative inline-flex items-center cursor-pointer">
                       <input
                         onClick={changeCategoryAllTogether}
                         type="checkbox"
                         value={category}
-                        class="sr-only peer"
+                        className="sr-only peer"
                       />
-                      <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                      <span class="ms-3 lg:text-lg lg:font-bold font-medium text-sm text-gray-900 dark:text-gray-300">
+                      <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                      <span className="ms-3 lg:text-lg lg:font-bold font-medium text-sm text-gray-900 dark:text-gray-300">
                         Movies
                       </span>
                     </label>
